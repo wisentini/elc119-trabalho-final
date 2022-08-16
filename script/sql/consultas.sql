d2095ee846c0c4d7a1aad5e9f62ce67081413f87
Marcos Visentini <marcosvisentini@gmail.com>
2022-08-16 17:26:29 -0300

    Adiciona consulta

--- script/sql/consultas.sql
+++ script/sql/consultas.sql
@@ -14,3 +14,18 @@ WHERE
     aa.nome = 'ENGENHARIA ELÉTRICA' AND
     m.sigla_uf = 'RS'
 ORDER BY nota_componente_especifico DESC;
+
+-- Ranking das médias do conceito enade por universidade pública
+-- O número de cursos participantes por universidade pública também
+-- compõe o resultado, já que ele influencia na média final
+SELECT
+    ies.nome AS nome_ies,
+    COUNT(*) AS num_cursos_part,
+    AVG(conceito_enade_faixa) AS media_conceito_enade
+FROM
+    conceito_enade AS ce NATURAL JOIN curso AS c NATURAL JOIN ies
+WHERE
+    ies.org_acad = "Universidade" AND
+    ies.categ_adm LIKE "Pública%"
+GROUP BY ies.cod_ies
+ORDER BY media_conceito_enade DESC;
